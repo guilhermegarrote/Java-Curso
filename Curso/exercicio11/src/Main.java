c91c18ddc4b5fb93c444733e8cb01ef52c41801c
Guilherme Garrote <guilhermegg2008@gmail.com>
2023-05-26 17:50:53 -0300

    Aula 59

--- Curso/exercicio11/src/Main.java
+++ Curso/exercicio11/src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite o número de linha que deseja: ");
         int n1 = sc.nextInt();
 
-        for (int n2 = 1; n1 >= n2; n2++) {
+            for (int n2 = 1; n1 >= n2; n2++) {
             int n3 = (int) Math.pow(n2, 2);
             int n4 = (int) Math.pow(n2, 3);
             System.out.println(n2 + " " + n3 + " " + n4);
